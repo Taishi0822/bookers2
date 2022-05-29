a8a7386ae3ecb81d73d587a1f39209c52dddba9e
EC2 Default User <ec2-user@ip-172-31-2-208.ec2.internal>
2022-05-29 04:49:39 +0000

    [Update]-bookers2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,7 @@
 class BooksController < ApplicationController
+  
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
+  
   def index
     @book = Book.new
     @books = Book.all
@@ -7,8 +10,13 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def show
@@ -29,8 +37,12 @@ class BooksController < ApplicationController
   
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(@book.id)
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
   
   
@@ -38,5 +50,12 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+  
+  def ensure_correct_user
+    @book = Book.find(params[:id])
+  unless @book.user == current_user
+    redirect_to books_path
+  end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
+  
+  before_action :ensure_correct_user, only: [:edit, :update]
+  
   def index
     @users = User.all
-   
+    @user = current_user
   end
 
   def show
@@ -10,8 +13,31 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+    flash[:notice] = "You have updated user successfully."
+    redirect_to  user_path(@user)
+    else
+    render :edit
+    end
     
   end
   
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+  
+  def ensure_correct_user
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to user_path(current_user)
+    end
+  end
   
 end
